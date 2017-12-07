ac30228821097810b036d26ddb985b3d51286bd0
Jorge Vásquez <30615979+jorge-vsqz@users.noreply.github.com>
2017-12-07 11:22:08 -0300

    Agrega archivos master y slave .ino

---
+++ master.ino
@@ -0,0 +1,39 @@
+
+int xPin = A1;
+int yPin = A0;
+int zPin = A2;
+
+int xPosition = 0;
+int yPosition = 0;
+int zPosition = 0;
+byte velDer, velIzq, velInf;
+
+void setup() {
+  // inicializar las comunicaciones en serie a 9600 bps:
+  Serial.begin(9600); 
+  
+  pinMode(xPin, INPUT);
+  pinMode(yPin, INPUT);
+  pinMode(zPin, INPUT);
+}
+
+void loop() {
+  xPosition = analogRead(xPin) - 509;
+  yPosition = 501 - analogRead(yPin);
+  zPosition = analogRead(zPin);
+  
+  if(xPosition>=0) {
+    velIzq = map(abs(yPosition), 0, 501, 0, 100);
+    velDer = map(abs(yPosition), 0, 501, 0, 100) - map(xPosition, 0, 514, 0, velIzq);
+  }
+  if(xPosition<0) {
+    velDer = map(abs(yPosition), 0, 501, 0, 100);
+    velIzq = map(abs(yPosition), 0, 501, 0, 100) - map(abs(xPosition), 0, 509, 0, velDer);
+  }
+  velInf = map(zPosition, 0, 1023, 0, 100);
+
+  byte outByte[4] = {255, velDer, velIzq, velInf};
+  Serial.write(outByte,4);
+  delay(10); // añadir un poco de retraso entre lecturas
+}
+
---
+++ slave.ino
@@ -0,0 +1,30 @@
+#include <Servo.h>
+
+Servo motIzq, motDer, motInf;
+int i;
+
+int vel[4] = {0,0,0,0};  // velocidades
+int inval[4];  // valores entrantes
+
+void setup() {
+  motIzq.attach(9);  // motor pin 9
+  motDer.attach(11);  // motor pin 11
+  motInf.attach(6);  // motor pin 6
+  Serial.begin(9600);  // serial a 9600 baud
+}
+
+void loop() {
+  while (Serial.available()<4) {}     // Esperar 4 bytes
+  for(int n=0; n<4; n++){
+    inval[n] = Serial.read();         // Leer los 4 bytes y ponerlos en un arreglo
+  }
+  if (inval[0] == 255) {               //
+    for (i=1; i<3; i++) {              //
+      vel[i] = map(inval[i], 0, 100, 1000, 2000);
+    }
+    vel[3] = map(inval[3], 0, 100, 1000, 2000);
+  }
+  motDer.writeMicroseconds(vel[1]);
+  motIzq.writeMicroseconds(vel[2]);
+  motInf.writeMicroseconds(vel[3]);
+}
